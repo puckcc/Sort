fb47351d429a3949ebbe2f495d65eb1123120617
蔡琛 <puckcc@gmail.com>
2015-07-10 16:40:14 +0800

    Update Sort.c

--- Sort.c
+++ Sort.c
@@ -18,8 +18,8 @@ typedef int Status;
 #define MAXSIZE 10000            /* 用于要排序数组个数最大值，可根据需要修改 */
 typedef struct
 {
-	int r[MAXSIZE+1];	         /* 用于存储要排序数组，r[0]用作哨兵或临时变量 */
-	int length;			         /* 用于记录+顺序表的长度 */
+	int r[MAXSIZE+1];	 /* 用于存储要排序数组，r[0]用作哨兵或临时变量 */
+	int length;		 /* 用于记录+顺序表的长度 */
 }SqList;
 
 /* 交换L中数组r的下标为i和j的值 */
@@ -49,7 +49,7 @@ void BubbleSort0(SqList *L)
 		{
 			if(L->r[i]>L->r[j])
 			{
-				 swap(L,i,j);        /* 交换L->r[i]与L->r[j]的值 */
+				 swap(L,i,j);   /* 交换L->r[i]与L->r[j]的值 */
 			}
 		}
 	}
@@ -61,11 +61,11 @@ void BubbleSort(SqList *L)
 	int i,j;
 	for(i=1;i<L->length;i++)
 	{
-		for(j=L->length-1;j>=i;j--)  /* 注意j是从后往前循环 */
+		for(j=L->length-1;j>=i;j--)     /* 注意j是从后往前循环 */
 		{
-			if(L->r[j]>L->r[j+1])    /* 若前者大于后者（注意这里与上一算法的差异）*/
+			if(L->r[j]>L->r[j+1])   /* 若前者大于后者（注意这里与上一算法的差异）*/
 			{
-				 swap(L,j,j+1);      /* 交换L->r[j]与L->r[j+1]的值 */
+				 swap(L,j,j+1); /* 交换L->r[j]与L->r[j+1]的值 */
 			}
 		}
 	}
@@ -75,16 +75,16 @@ void BubbleSort(SqList *L)
 void BubbleSort2(SqList *L)
 { 
 	int i,j;
-	Status flag=TRUE;			     /* flag用来作为标记 */
+	Status flag=TRUE;		  /* flag用来作为标记 */
 	for(i=1;i<L->length && flag;i++) /* 若flag为true说明有过数据交换，否则停止循环 */
 	{
-		flag=FALSE;				     /* 初始为False */
+		flag=FALSE;		 /* 初始为False */
 		for(j=L->length-1;j>=i;j--)
 		{
 			if(L->r[j]>L->r[j+1])
 			{
-				 swap(L,j,j+1);	     /* 交换L->r[j]与L->r[j+1]的值 */
-				 flag=TRUE;		     /* 如果有数据交换，则flag为true */
+				 swap(L,j,j+1);	 /* 交换L->r[j]与L->r[j+1]的值 */
+				 flag=TRUE;	 /* 如果有数据交换，则flag为true */
 			}
 		}
 	}
@@ -97,14 +97,14 @@ void SelectSort(SqList *L)
 	int i,j,min;
 	for(i=1;i<L->length;i++)
 	{ 
-		min = i;						/* 将当前下标定义为最小值下标 */
+		min = i;		        /* 将当前下标定义为最小值下标 */
 		for (j = i+1;j<=L->length;j++)  /* 循环之后的数据 */
         {
-			if (L->r[min]>L->r[j])	    /* 如果有小于当前最小值的关键字 */
-                min = j;				/* 将此关键字的下标赋值给min */
+			if (L->r[min]>L->r[j])  /* 如果有小于当前最小值的关键字 */
+                min = j;			/* 将此关键字的下标赋值给min */
         }
-		if(i!=min)						/* 若min不等于i，说明找到最小值，交换 */
-			swap(L,i,min);				/* 交换L->r[i]与L->r[min]的值 */
+		if(i!=min)			/* 若min不等于i，说明找到最小值，交换 */
+			swap(L,i,min);		/* 交换L->r[i]与L->r[min]的值 */
 	}
 }
 
@@ -114,7 +114,7 @@ void InsertSort(SqList *L)
 	int i,j;
 	for(i=2;i<=L->length;i++)
 	{
-		if (L->r[i]<L->r[i-1])      /* 需将L->r[i]插入有序子表 */
+		if (L->r[i]<L->r[i-1])          /* 需将L->r[i]插入有序子表 */
 		{
 			L->r[0]=L->r[i];        /* 设置哨兵 */
 			for(j=i-1;L->r[j]>L->r[0];j--)
@@ -131,12 +131,12 @@ void ShellSort(SqList *L)
 	int increment=L->length;
 	do
 	{
-		increment=increment/3+1;               /* 增量序列 */
+		increment=increment/3+1;                   /* 增量序列 */
 		for(i=increment+1;i<=L->length;i++)
 		{
 			if (L->r[i]<L->r[i-increment])     /*  需将L->r[i]插入有序增量子表 */ 
 			{ 
-				L->r[0]=L->r[i];               /*  暂存在L->r[0] */
+				L->r[0]=L->r[i];           /*  暂存在L->r[0] */
 				for(j=i-increment;j>0 && L->r[0]<L->r[j];j-=increment)
 					L->r[j+increment]=L->r[j]; /*  记录后移，查找插入位置 */
 				L->r[j+increment]=L->r[0];     /*  插入 */
